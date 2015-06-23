a931763defab1ebda2fb9aa0c47042fca1226aeb
shanikawm <shanikawm@gmail.com>
2015-06-23 15:35:18 +0530

    Final Tested version example.php added.

--- actext.php
+++ actext.php
@@ -1,49 +1,52 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: Shanika
  * Date: 6/23/2015
  * Time: 2:10 PM
  */
 
-namespace actext;
+namespace AutoCompleteText;
 
 
-class ac_text {
-    private  $haystack;
-    function __construct($h){
-        $this->haystack=$h;
+class ac_text
+{
+    private $haystack;
+
+    function __construct($h)
+    {
+        $this->haystack = $h;
     }
 
-    public function search($term,$limit=5){
-        $term=strtolower($term);
-        $result=array_values(preg_grep('/^'.$term.'/i',$this->haystack));
-        if(count($result)<$limit){
-            $result=array_merge($result,array_values(preg_grep('/ '.$term.'/i',$this->haystack)));
+    public function search($needle, $limit = 5)
+    {
+        $needle = strtolower($needle);
+        $result = array_values(preg_grep('/^' . $needle . '/i', $this->haystack));
+        if (count($result) < $limit) {
+            $result = array_merge($result, array_values(preg_grep('/ ' . $needle . '/i', $this->haystack)));
         }
-        if(count($result)<$limit){
-            $similar=array();
-            foreach($this->haystack as $c){
-                $sc=explode(' ',$c);
-                if(count($sc)>1){
-                    foreach($sc as $scp){
-                        similar_text(strtolower($scp),$term,$p);
-                        $similar[$c]=max($p,$similar[$c]);
+        if (count($result) < $limit) {
+            $similar = array();
+            foreach ($this->haystack as $h) {
+                $h_split = explode(' ', $h);
+                if (count($h_split) > 1) {
+                    foreach ($h_split as $hs) {
+                        similar_text(strtolower($hs), $needle, $p);
+                        $similar[$h] = max($p, $similar[$h]);
                     }
                 } else {
-                    similar_text(strtolower($c),$term,$p);
-                    $similar[$c]=$p;
+                    similar_text(strtolower($h), $needle, $p);
+                    $similar[$h] = $p;
                 }
-        }   
-        arsort($similar);
-        $similar=array_keys($similar);
-        while(count($result)<$limit){
-            $e=array_shift($similar);
-            if(!in_array($e,$result)){
-                $result[]=$e;
+            }
+            arsort($similar);
+            $similar = array_keys($similar);
+            while (count($result) < $limit) {
+                $e = array_shift($similar);
+                if (!in_array($e, $result)) {
+                    $result[] = $e;
+                }
             }
         }
-        }
-    return $result;
+        return $result;
     }
 }
---
+++ example.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * User: Shanika
+ * Date: 6/23/2015
+ * Time: 3:17 PM
+ */
+include_once 'actext.php';
+$ac=new AutoCompleteText\ac_text([
+    'Light Pink',
+    'Pale Violet Red',
+    'Light Salmon',
+    'Fire Brick',
+    'Medium Spring Green',
+    'Dark Olive Green',
+    'Medium Slate Blue',
+    'Pale Turquoise',
+    'Light Steel Blue',
+    'Light Slate Gray',
+    'Dark Goldenrod',
+    'Corn flower Blue',
+    'Antique White',
+    'Navajo White',
+    'Blanched Almond',
+    'Burly Wood',
+    'Powder Blue',
+    'Dark Olive Green'
+]);
+echo "---Blue---\n"; //Search for existing word
+print_r($ac->search('blue'));
+echo "---Water---\n"; //Search for non existing word
+print_r($ac->search('Water'));
+echo "--hwite---\n"; //Search for spelling mistake word
+print_r($ac->search('hwite'));
