b8194896e09d829ef52b34cdae9825eeb5fc7bbc
shanikawm <shanikawm@users.noreply.github.com>
2015-06-23 14:55:04 +0530

    Update actext.php

--- actext.php
+++ actext.php
@@ -15,7 +15,35 @@ class ac_text {
         $this->haystack=$h;
     }
 
-    public function search(){
-        return array();
+    public function search($term,$limit=5){
+        $term=strtolower($term);
+        $result=array_values(preg_grep('/^'.$term.'/i',$this->haystack));
+        if(count($result)<$limit){
+            $result=array_merge($result,array_values(preg_grep('/ '.$term.'/i',$this->haystack)));
+        }
+        if(count($result)<$limit){
+            $similar=array();
+            foreach($this->haystack as $c){
+                $sc=explode(' ',$c);
+                if(count($sc)>1){
+                    foreach($sc as $scp){
+                        similar_text(strtolower($scp),$term,$p);
+                        $similar[$c]=max($p,$similar[$c]);
+                    }
+                } else {
+                    similar_text(strtolower($c),$term,$p);
+                    $similar[$c]=$p;
+                }
+        }   
+        arsort($similar);
+        $similar=array_keys($similar);
+        while(count($result)<$limit){
+            $e=array_shift($similar);
+            if(!in_array($e,$result)){
+                $result[]=$e;
+            }
+        }
+        }
+    return $result;
     }
 }
